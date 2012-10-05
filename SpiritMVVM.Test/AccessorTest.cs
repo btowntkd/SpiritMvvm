4caf8c809a287a2e592506698212fb854c6df52c
Brian Pratt <btowntkd@gmail.com>
2012-10-04 22:31:52 -0400

    -Initial solution structure created. -Initial .gitignore list created. -Accessor class added. -Unit Test project created. -Moq added to Unit Test project.

---
+++ SpiritMVVM.Test/AccessorTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpiritMVVM.Test
+{
+    [TestClass]
+    public class AccessorTest
+    {
+        /// <summary>
+        /// Ensures that a constructor called with a null Getter will throw an <see cref="ArgumentNullException"/>.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_GetterIsNull_ThrowsException()
+        {
+            try
+            {
+                int backingStore;
+                Accessor<int> accessor = new Accessor<int>(null, (x) => backingStore = x);
+                Assert.Fail("Expected exception to be thrown.");
+            }
+            catch (ArgumentNullException)
+            {
+                //PASS
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception does not match expected type.");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that a constructor called with a null Setter will throw an <see cref="ArgumentNullException"/>.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_SetterIsNull_ThrowsException()
+        {
+            try
+            {
+                int backingStore = 0;
+                Accessor<int> accessor = new Accessor<int>(() => backingStore, null);
+                Assert.Fail("Expected exception to be thrown.");
+            }
+            catch (ArgumentNullException)
+            {
+                //PASS
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception does not match expected type.");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that a constructor called with valid arguments will not throw an exception.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_ValidArgs_Success()
+        {
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+        }
+
+        /// <summary>
+        /// Ensures that getting the Value property calls the given getter method.
+        /// </summary>
+        [TestMethod]
+        public void GetValue_CallsGetter()
+        {
+            bool getterCalled = false;
+            Accessor<int> accessor = new Accessor<int>(
+            () => { getterCalled = true; return 0; },
+            (x) => { /* Do nothing */ });
+
+            //Call the Value's "Get" method
+            int getVal = accessor.Value;
+
+            Assert.IsTrue(getterCalled, "Expected Getter method to be called");
+        }
+
+        /// <summary>
+        /// Ensures that setting the Value property calls the given setter method.
+        /// </summary>
+        [TestMethod]
+        public void SetValue_CallsSetter()
+        {
+            bool setterCalled = false;
+            Accessor<int> accessor = new Accessor<int>(
+                () => { return 0; },
+                (x) => { setterCalled = true; });
+
+            //Call the Value's "Set" method
+            accessor.Value = 0;
+
+            Assert.IsTrue(setterCalled, "Expected Setter method to be called");
+        }
+    }
+}
---
+++ SpiritMVVM/Accessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SpiritMVVM
+{
+    /// <summary>
+    /// Contains Getter and Setter methods for an externally-referenced value.
+    /// </summary>
+    public class Accessor<T>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Accessor{T}"/> object, with the given Getter and Setter methods.
+        /// </summary>
+        /// <param name="getter">The method to use when retrieving the item's value.</param>
+        /// <param name="setter">The method to use when setting the item's value.</param>
+        public Accessor(Func<T> getter, Action<T> setter)
+        {
+            if (getter == null)
+            {
+                throw new ArgumentNullException("getter");
+            }
+            if (setter == null)
+            {
+                throw new ArgumentNullException("setter");
+            }
+
+            Getter = getter;
+            Setter = setter;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Get or Set the underlying value using the getter or setter methods.
+        /// </summary>
+        public T Value
+        {
+            get { return Getter(); }
+            set { Setter(value); }
+        }
+
+        /// <summary>
+        /// Get the Getter method.
+        /// </summary>
+        public Action<T> Setter { get; private set; }
+
+        /// <summary>
+        /// Get the Setter method
+        /// </summary>
+        public Func<T> Getter { get; private set; }
+
+        #endregion
+    }
+}
