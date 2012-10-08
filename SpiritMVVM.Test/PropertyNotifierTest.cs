9c647319bb8968d1c0cf15c9c5da4f38b6438054
Brian Pratt <btowntkd@gmail.com>
2012-10-08 19:51:06 -0400

    Added "PropertyNotifier" and unit tests.

---
+++ SpiritMVVM.Test/PropertyNotifierTest.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpiritMVVM.Test
+{
+    [TestClass]
+    public class PropertyNotifierTest
+    {
+        #region Constructor Tests
+
+        [TestMethod]
+        public void Constructor_NullAction_ThrowsException()
+        {
+            try
+            {
+                PropertyNotifier notifier = new PropertyNotifier(null);
+            }
+            catch (ArgumentNullException)
+            {
+                //PASS
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Exception was not the expected type.");
+            }
+        }
+
+        [TestMethod]
+        public void Constructor_ValidArgs_Success()
+        {
+            PropertyNotifier notifier = new PropertyNotifier((s) => { /* Do nothing */ });
+        }
+
+        #endregion
+
+        #region Set (ref Argument) Tests
+
+        [TestMethod]
+        public void Set_WithRefArgument_Changed_ExecutesNotifier()
+        {
+            bool notifierExecuted = false;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                notifierExecuted = true;
+            });
+
+            int backingStore = 0;
+            int newValue = 12;
+            notifier.SetProperty(ref backingStore, newValue);
+            
+            Assert.IsTrue(notifierExecuted, "Expected notifier delegate to be executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithRefArgument_NotChanged_DoesNotExecuteNotifier()
+        {
+            bool notifierExecuted = false;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                notifierExecuted = true;
+            });
+
+            int backingStore = 0;
+            int newValue = 0;
+            notifier.SetProperty(ref backingStore, newValue);
+
+            Assert.IsFalse(notifierExecuted, "Did not expect notifier delegate to be executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithRefArgument_Changed_ProvidesCorrectPropertyNameToNotifier()
+        {
+            //Use a guid as a property name, to ensure randomness
+            string expectedPropertyName = Guid.NewGuid().ToString();
+            string receivedPropertyName = string.Empty;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                receivedPropertyName = propName;
+            });
+
+            int backingStore = 0;
+            int newValue = 12;
+            notifier.SetProperty(ref backingStore, newValue, null, expectedPropertyName);
+
+            Assert.AreEqual(expectedPropertyName, receivedPropertyName, "Property names do not match.");
+        }
+
+        [TestMethod]
+        public void Set_WithRefArgumentAndCallback_Changed_ExecutesCallback()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            bool callbackExecuted = false;
+            int backingStore = 0;
+            int newValue = 12;
+            notifier.SetProperty(ref backingStore, newValue, (oldVal, newVal) =>
+            {
+                callbackExecuted = true;
+            });
+
+            Assert.IsTrue(callbackExecuted, "Callback was not executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithRefArgumentAndCallback_NotChanged_DoesNotExecuteCallback()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            bool callbackExecuted = false;
+            int backingStore = 12;
+            int newValue = 12;
+            notifier.SetProperty(ref backingStore, newValue, (oldVal, newVal) =>
+            {
+                callbackExecuted = true;
+            });
+
+            Assert.IsFalse(callbackExecuted, "Callback was executed, but shouldn't.");
+        }
+
+        [TestMethod]
+        public void Set_WithRefArgumentAndCallback_Changed_ProvidesCorrectOldAndNewValues()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            int expectedOldValue = 0;
+            int expectedNewValue = 12;
+            int receivedOldValue = -1;
+            int receivedNewValue = -1;
+            int backingStore = 0;
+            notifier.SetProperty(ref backingStore, expectedNewValue, (oldVal, newVal) =>
+            {
+                receivedOldValue = oldVal;
+                receivedNewValue = newVal;
+            });
+
+            Assert.AreEqual(expectedOldValue, receivedOldValue, "'Old' values do not match.");
+            Assert.AreEqual(expectedNewValue, receivedNewValue, "'New' values do not match.");
+        }
+
+        #endregion
+
+        #region Set (with Accessor<T>) Tests
+
+        [TestMethod]
+        public void Set_WithAccessor_Changed_ExecutesNotifier()
+        {
+            bool notifierExecuted = false;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                notifierExecuted = true;
+            });
+
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            int newValue = 12;
+            notifier.SetProperty(accessor, newValue);
+
+            Assert.IsTrue(notifierExecuted, "Expected notifier delegate to be executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithAccessor_NotChanged_DoesNotExecuteNotifier()
+        {
+            bool notifierExecuted = false;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                notifierExecuted = true;
+            });
+
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            int newValue = 0;
+            notifier.SetProperty(accessor, newValue);
+
+            Assert.IsFalse(notifierExecuted, "Did not expect notifier delegate to be executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithAccessor_Changed_ProvidesCorrectPropertyNameToNotifier()
+        {
+            //Use a guid as a property name, to ensure randomness
+            string expectedPropertyName = Guid.NewGuid().ToString();
+            string receivedPropertyName = string.Empty;
+            PropertyNotifier notifier = new PropertyNotifier((propName) =>
+            {
+                receivedPropertyName = propName;
+            });
+
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            int newValue = 12;
+            notifier.SetProperty(accessor, newValue, null, expectedPropertyName);
+
+            Assert.AreEqual(expectedPropertyName, receivedPropertyName, "Property names do not match.");
+        }
+
+        [TestMethod]
+        public void Set_WithAccessor_Changed_ExecutesCallback()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            bool callbackExecuted = false;
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            int newValue = 12;
+            notifier.SetProperty(accessor, newValue, (oldVal, newVal) =>
+            {
+                callbackExecuted = true;
+            });
+
+            Assert.IsTrue(callbackExecuted, "Callback was not executed.");
+        }
+
+        [TestMethod]
+        public void Set_WithAccessor_NotChanged_DoesNotExecuteCallback()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            bool callbackExecuted = false;
+            int backingStore = 12;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            int newValue = 12;
+            notifier.SetProperty(accessor, newValue, (oldVal, newVal) =>
+            {
+                callbackExecuted = true;
+            });
+
+            Assert.IsFalse(callbackExecuted, "Callback was executed, but shouldn't.");
+        }
+
+        [TestMethod]
+        public void Set_WithAccessor_Changed_ProvidesCorrectOldAndNewValues()
+        {
+            //Use a guid as a property name, to ensure randomness
+            PropertyNotifier notifier = new PropertyNotifier((propName) => { /* Do nothing */ });
+
+            int expectedOldValue = 0;
+            int expectedNewValue = 12;
+            int receivedOldValue = -1;
+            int receivedNewValue = -1;
+            int backingStore = 0;
+            Accessor<int> accessor = new Accessor<int>(() => backingStore, (x) => backingStore = x);
+            notifier.SetProperty(accessor, expectedNewValue, (oldVal, newVal) =>
+            {
+                receivedOldValue = oldVal;
+                receivedNewValue = newVal;
+            });
+
+            Assert.AreEqual(expectedOldValue, receivedOldValue, "'Old' values do not match.");
+            Assert.AreEqual(expectedNewValue, receivedNewValue, "'New' values do not match.");
+        }
+
+        #endregion
+    }
+}
---
+++ SpiritMVVM/PropertyNotifier.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiritMVVM
+{
+    /// <summary>
+    /// Utility class used as a proxy when assigning values to properties,
+    /// in order to automatically raise property-change notification events.
+    /// </summary>
+    public class PropertyNotifier
+    {
+        private Action<string> _propertyNotificationAction = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyChangedAction"></param>
+        public PropertyNotifier(Action<string> propertyChangedAction)
+        {
+            if (propertyChangedAction == null)
+                throw new ArgumentNullException("propertyChangedAction");
+
+            _propertyNotificationAction = propertyChangedAction;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="backingStore"></param>
+        /// <param name="newValue"></param>
+        /// <param name="onChangedCallback"></param>
+        /// <param name="propertyName"></param>
+        public void SetProperty<T>(ref T backingStore, T newValue, Action<T, T> onChangedCallback = null, [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(backingStore, newValue))
+            {
+                //Store the old value for the callback
+                T oldValue = backingStore;
+                backingStore = newValue;
+
+                //Execute the callback, if one was provided
+                if (onChangedCallback != null)
+                    onChangedCallback(oldValue, newValue);
+
+                //Raise the notification event.
+                RaiseNotification(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="backingStore"></param>
+        /// <param name="newValue"></param>
+        /// <param name="onChangedCallback"></param>
+        /// <param name="propertyName"></param>
+        public void SetProperty<T>(Accessor<T> backingStore, T newValue, Action<T, T> onChangedCallback = null, [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(backingStore.Value, newValue))
+            {
+                //Store the old value for the callback
+                T oldValue = backingStore.Value;
+                backingStore.Value = newValue;
+
+                //Execute the callback, if one was provided
+                if (onChangedCallback != null)
+                    onChangedCallback(oldValue, newValue);
+
+                //Raise the notification event.
+                RaiseNotification(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void RaiseNotification(string propertyName)
+        {
+            var handler = _propertyNotificationAction;
+            if (handler != null)
+            {
+                handler(propertyName);
+            }
+        }
+    }
+}
