bcf58f8b658a5954823b2804f89aaa44e6b8cac6
Brian Pratt <btowntkd@gmail.com>
2012-10-24 16:30:26 -0400

    Added unit tests for SelectionViewModel.

--- SpiritMVVM.Test/ViewModels/SelectionViewModelTest.cs
+++ SpiritMVVM.Test/ViewModels/SelectionViewModelTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpiritMVVM.ViewModels;
 
 namespace SpiritMVVM.Test.ViewModels
@@ -10,6 +9,115 @@ namespace SpiritMVVM.Test.ViewModels
     [TestClass]
     public class SelectionViewModelTest
     {
-        //TODO: insert tests
+        /// <summary>
+        /// Ensures the default constructor is successful.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_Default_Success()
+        {
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>();
+        }
+
+        /// <summary>
+        /// Ensures the default constructor has a default IsSelected value of false.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_Default_IsSelectedIsFalse()
+        {
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>();
+            Assert.IsFalse(viewModel.IsSelected, "Expected IsSelected to be false by default.");
+        }
+
+        /// <summary>
+        /// Ensures the default constructor has a default Value property of equal to default(T).
+        /// </summary>
+        [TestMethod]
+        public void Constructor_Default_ItemIsDefaultClassInstance()
+        {
+            //Repeat with a couple different types
+            Constructor_Default_ItemIsDefaultInstanceOfT<int>();
+            Constructor_Default_ItemIsDefaultInstanceOfT<object>();
+            Constructor_Default_ItemIsDefaultInstanceOfT<System.Net.IPAddress>();
+            Constructor_Default_ItemIsDefaultInstanceOfT<string>();
+        }
+
+        /// <summary>
+        /// Executes the Constructor_Default_ItemIsDefaultClassInstance test
+        /// for the specified type.
+        /// </summary>
+        /// <typeparam name="TItem">The type for which to execute the unit test.</typeparam>
+        private void Constructor_Default_ItemIsDefaultInstanceOfT<TItem>()
+        {
+            SelectionViewModel<TItem> viewModel = new SelectionViewModel<TItem>();
+            Assert.AreEqual(viewModel.Item, default(TItem), "Expected Value to be the default class instance.");
+        }
+
+        /// <summary>
+        /// Ensures the constructor with an initial isSelected parameter sets
+        /// the IsSelected property to the specified state.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_WithItem_AssignsItemProperty()
+        {
+            int expectedValue = 12;
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>(expectedValue);
+            Assert.AreEqual(expectedValue, viewModel.Item, "Expected Item property to equal the constructor parameter.");
+        }
+
+        /// <summary>
+        /// Ensures the constructor with an initial isSelected parameter sets
+        /// the IsSelected property to the specified state.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_WithItemAndInitialState_AssignsItemPropertyAndInitialState()
+        {
+            int expectedValue = 12;
+            bool initialState = false;
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>(expectedValue, initialState);
+            Assert.AreEqual(expectedValue, viewModel.Item, "Expected Item property to equal the constructor parameter.");
+            Assert.AreEqual(initialState, viewModel.IsSelected, "Expected IsSelected property to equal the constructor parameter.");
+
+            expectedValue = 32;
+            initialState = true;
+            viewModel = new SelectionViewModel<int>(expectedValue, initialState);
+            Assert.AreEqual(expectedValue, viewModel.Item, "Expected Item property to equal the constructor parameter.");
+            Assert.AreEqual(initialState, viewModel.IsSelected, "Expected IsSelected property to equal the constructor parameter.");
+        }
+
+        /// <summary>
+        /// Ensures that the IsSelected property raises the PropertyChanged event,
+        /// when modified.
+        /// </summary>
+        [TestMethod]
+        public void IsSelected_Changed_RaisesPropertyChangedEvent()
+        {
+            bool pass = false;
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>();
+            viewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "IsSelected")
+                    pass = true;
+            };
+            viewModel.IsSelected = !viewModel.IsSelected;
+            Assert.IsTrue(pass, "Expected PropertyChanged event, which was never raised.");
+        }
+
+        /// <summary>
+        /// Ensures that the Item property raises the PropertyChanged event,
+        /// when modified.
+        /// </summary>
+        [TestMethod]
+        public void Item_Changed_RaisesPropertyChangedEvent()
+        {
+            bool pass = false;
+            SelectionViewModel<int> viewModel = new SelectionViewModel<int>();
+            viewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Item")
+                    pass = true;
+            };
+            viewModel.Item = 34;
+            Assert.IsTrue(pass, "Expected PropertyChanged event, which was never raised.");
+        }
     }
 }
