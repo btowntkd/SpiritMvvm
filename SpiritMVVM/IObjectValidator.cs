6dca8c801af6479c0e28fe69a52be5e41d80f09d
Brian Pratt <btowntkd@gmail.com>
2012-11-09 15:59:42 -0500

    Started fleshing out the structure of the injectable validation functionality. Added IObjectValidator utility interface.

---
+++ SpiritMVVM/IObjectValidator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SpiritMVVM
+{
+    /// <summary>
+    /// An interface used for validating a parent class instance.
+    /// </summary>
+    /// <typeparam name="TParent">The parent class which the interface will validate.</typeparam>
+    public interface IObjectValidator<TParent>
+        where TParent : IValidatableObject
+    {
+        /// <summary>
+        /// Validate the entire object instance.
+        /// </summary>
+        /// <param name="instance">The instance to validate.</param>
+        void Validate(TParent instance);
+
+        /// <summary>
+        /// Validate the given property name of the object.
+        /// </summary>
+        /// <param name="instance">The instance to validate.</param>
+        /// <param name="propertyName">The name of the property to validate.</param>
+        void Validate(TParent instance, string propertyName);
+    }
+}
--- SpiritMVVM/IValidatableObject.cs
+++ SpiritMVVM/IValidatableObject.cs
@@ -12,9 +12,23 @@ namespace SpiritMVVM
     /// </summary>
     public interface IValidatableObject : INotifyDataErrorInfo
     {
+        /// <summary>
+        /// Get the value indicating whether the current instance is valid
+        /// (i.e. contains no validation errors).
+        /// </summary>
         bool IsValid { get; }
 
+        /// <summary>
+        /// Add an error description, associated with the given property name.
+        /// </summary>
+        /// <param name="propertyName">The name of the invalid property.</param>
+        /// <param name="error">A description of the error.</param>
         void AddError(string propertyName, string error);
+
+        /// <summary>
+        /// Clear any errors from the given property name.
+        /// </summary>
+        /// <param name="propertyName">The property for which to clear any errors.</param>
         void ClearErrors(string propertyName);
     }
 }
