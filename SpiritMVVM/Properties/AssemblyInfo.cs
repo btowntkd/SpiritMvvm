d7e8d0ea20a0e83f69aa31ba04a63a1119250e95
Brian Pratt <bcpratt@live.com>
2013-10-25 15:42:03 -0400

    Updated copyright.

--- SpiritMVVM/Properties/AssemblyInfo.cs
+++ SpiritMVVM/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpiritMVVM")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
